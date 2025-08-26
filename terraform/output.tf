4bbaff8c4e3b53671b870897f90fdf19e31d5831
AyushChoudhary6 <ayushrjchoudhary2005@gmail.com>
2025-08-26 22:12:22 +0530

    fixedsd more bugs

---
+++ terraform/output.tf
@@ -0,0 +1,7 @@
+output "eks_cluster_name" {
+  value = module.eks.cluster_id
+}
+
+# output "db_endpoint" {
+#   value = module.db.endpoint
+# }
---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+# variable "aws_access_key" {}
+# variable "aws_secret_key" {}
+
+variable "cluster_name" {
+  default = "chat-app-cluster"
+}
